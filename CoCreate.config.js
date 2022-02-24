f0f1bb4cd1157021f2196c2ef0ad42095de65fa6
Frank <frank@cocreate.app>
2022-02-24 19:10:28 +0000

    fix: CoCreate.config replace CoCreate.app with *

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/remove/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/remove",
