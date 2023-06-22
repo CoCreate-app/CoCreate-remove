cee1ec37aa6cde91b5149122b3410577f93bf7a6
frankpagan <frank@cocreate.app>
2023-06-22 15:26:49 -0600

    fix: replace -target -selector

--- src/index.js
+++ src/index.js
@@ -1,89 +1,89 @@
 /*global CustomEvent*/
 import action from '@cocreate/actions';
 import text from '@cocreate/text';
-import {queryDocumentSelectorAll} from '@cocreate/utils';
+import { queryDocumentSelectorAll } from '@cocreate/utils';
 
-function  remove(btn) {
-	let elements;
-	let selector  = btn.getAttribute('remove-target');
+function remove(btn) {
+    let elements;
+    let selector = btn.getAttribute('remove-selector');
 
-	if (selector) {
-		elements = queryDocumentSelectorAll(selector);
-	} 
-	else {
-		selector = btn.getAttribute('remove-closest');
-		if (selector)
-			elements = [btn.closest(selector)];
-		else
-			elements = [btn.closest('[templateid]')];
-	}
-	let attribute = btn.getAttribute('remove-attribute');
-	let attributeValue = btn.getAttribute('remove-value');
+    if (selector) {
+        elements = queryDocumentSelectorAll(selector);
+    }
+    else {
+        selector = btn.getAttribute('remove-closest');
+        if (selector)
+            elements = [btn.closest(selector)];
+        else
+            elements = [btn.closest('[templateid]')];
+    }
+    let attribute = btn.getAttribute('remove-attribute');
+    let attributeValue = btn.getAttribute('remove-value');
 
-	for(let element of elements){
+    for (let element of elements) {
         let domTextEditor = element.closest('[contenteditable]');
-		if (attribute){
-			if (attributeValue) {
-				switch(attribute) {
-					case 'class':
-						if (domTextEditor)
-							text.removeClass({
-				        		domTextEditor,
-				        		target: element,
-				        		className: attributeValue
-				        	});
-				        else
-				        	element.classList.remove(attributeValue);
-						break;
-					case 'style':
-						if (domTextEditor)
-							text.removeStyle({
-				        		domTextEditor,
-				        		target: element,
-				        		property: attributeValue
-				        	});
-				        else
-				        	element.style.removeProperty(attributeValue);
-						break;
-					default:
-						if (domTextEditor)
-							text.setAttribute({
-				        		domTextEditor,
-				        		target: element,
-				        		name: attribute
-				        	});
-				        else
-				        	element.setAttribute(attribute, '');
-				}
-			}
-			else {
-				if (domTextEditor)
-					text.removeAttribute({
-		        		domTextEditor,
-		        		target: element,
-		        		name: attribute
-		        	});
-				else	
-					element.removeAttribute(attribute);
-			}
-		}
-		else {
-	        if (domTextEditor)
-	        	text.removeElement({
-	        		domTextEditor,
-	        		target: element
-	        	});
-		    else
-		        element.remove();
-		}
-	}
-	document.dispatchEvent(new CustomEvent('remove', {detail: {}}));
+        if (attribute) {
+            if (attributeValue) {
+                switch (attribute) {
+                    case 'class':
+                        if (domTextEditor)
+                            text.removeClass({
+                                domTextEditor,
+                                target: element,
+                                className: attributeValue
+                            });
+                        else
+                            element.classList.remove(attributeValue);
+                        break;
+                    case 'style':
+                        if (domTextEditor)
+                            text.removeStyle({
+                                domTextEditor,
+                                target: element,
+                                property: attributeValue
+                            });
+                        else
+                            element.style.removeProperty(attributeValue);
+                        break;
+                    default:
+                        if (domTextEditor)
+                            text.setAttribute({
+                                domTextEditor,
+                                target: element,
+                                name: attribute
+                            });
+                        else
+                            element.setAttribute(attribute, '');
+                }
+            }
+            else {
+                if (domTextEditor)
+                    text.removeAttribute({
+                        domTextEditor,
+                        target: element,
+                        name: attribute
+                    });
+                else
+                    element.removeAttribute(attribute);
+            }
+        }
+        else {
+            if (domTextEditor)
+                text.removeElement({
+                    domTextEditor,
+                    target: element
+                });
+            else
+                element.remove();
+        }
+    }
+    document.dispatchEvent(new CustomEvent('remove', { detail: {} }));
 }
 
 action.init({
-	name: "remove",
-	endEvent: "remove",
-	callback: (data) => {
-		remove(data.element);
-	}
+    name: "remove",
+    endEvent: "remove",
+    callback: (data) => {
+        remove(data.element);
+    }
 });
